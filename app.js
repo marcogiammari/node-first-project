c2e1775c44de726533939b08f468b4bc3bfb793d
Marco Giammari <marcogiammari@gmail.com>
2023-10-09 22:46:16 +0200

    adding a 404 error page

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
