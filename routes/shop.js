4df309181e8f2cbb8a488f30021c93eed4bd6622
Marco Giammari <marcogiammari@gmail.com>
2023-10-10 18:48:30 +0200

    outputting dynamic content

--- routes/shop.js
+++ routes/shop.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 const express = require("express");
 
+const products = require("./admin");
+
 const rootDir = path.dirname(require.main.filename);
 
 const adminData = require("./admin");
@@ -9,7 +11,8 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.render("shop");
+    const products = adminData.products;
+    res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
