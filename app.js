7c028785917c610ff8dc83cd5c55f211f3258e75
Marco Giammari <marcogiammari@gmail.com>
2023-10-10 20:16:23 +0200

    converting html files to pug

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.use((req, res) => {
+    res.status(404).render("404");
 });
 
 app.listen(3000);
