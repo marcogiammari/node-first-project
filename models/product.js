2ba79c6ba60aea7a8a0297a9fff8b3e8052426bc
Marco Giammari <marcogiammari@gmail.com>
2023-10-11 17:23:43 +0200

    storing data via the model

--- models/product.js
+++ models/product.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
 module.exports = class Product {
@@ -6,10 +9,34 @@ module.exports = class Product {
 	}
 
 	save() {
-		products.push(this);
+		const p = path.join(
+			path.dirname(process.mainModule.filename),
+			"data",
+			"products.json"
+		);
+		fs.readFile(p, (err, fileContent) => {
+			let products = [];
+			if (!err && fileContent.length > 0) {
+				products = JSON.parse(fileContent);
+			}
+			products.push(this);
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				console.log(err);
+			});
+		});
 	}
 
 	static fetchAll() {
-		return products;
+		const p = path.join(
+			path.dirname(process.mainModule.filename),
+			"data",
+			"products.json"
+		);
+		fs.readFile(p, (err, fileContent) => {
+			if (err) {
+				return [];
+			}
+			return JSON.parse(fileContent);
+		});
 	}
 };
