c35a9289a652959523e9ea44e2f0e14b83e326b4
Marco Giammari <marcogiammari@gmail.com>
2023-10-15 10:34:18 +0200

    creating & fetching a cart

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-	.sync({ force: true })
-	// .sync()
+	// .sync({ force: true })
+	.sync()
 	.then((res) => {
 		// console.log(res);
 		return User.findByPk(1);
@@ -62,6 +62,9 @@ sequelize
 	})
 	.then((user) => {
 		// console.log(user);
+		return user.createCart();
+	})
+	.then((cart) => {
 		app.listen(3000);
 	})
 	.catch((err) => {
