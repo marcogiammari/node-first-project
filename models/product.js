31c9c8a7d28714dcdf6d42216fc5e478659e7f62
Marco Giammari <marcogiammari@gmail.com>
2023-10-15 19:20:28 +0200

    creating the mongodb connection

--- models/product.js
+++ models/product.js
@@ -1,26 +1,36 @@
-const Sequelize = require("sequelize");
-const sequelize = require("../util/database");
+const getDb = require("../util/database").getDb;
 
-const Product = sequelize.define("product", {
-	id: {
-		type: Sequelize.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true,
-	},
-	title: Sequelize.STRING,
-	price: {
-		type: Sequelize.DOUBLE,
-		allowNull: false,
-	},
-	imageUrl: {
-		type: Sequelize.STRING,
-		allowNull: false,
-	},
-	description: {
-		type: Sequelize.STRING,
-		allowNull: false,
-	},
-});
+class Product {
+	constructor(title, price, description, imageUrl) {
+		this.title = title;
+		this.price = price;
+		this.description = description;
+		this.imageUrl = imageUrl;
+	}
+
+	save() {}
+}
+
+// const Product = sequelize.define("product", {
+// 	id: {
+// 		type: Sequelize.INTEGER,
+// 		autoIncrement: true,
+// 		allowNull: false,
+// 		primaryKey: true,
+// 	},
+// 	title: Sequelize.STRING,
+// 	price: {
+// 		type: Sequelize.DOUBLE,
+// 		allowNull: false,
+// 	},
+// 	imageUrl: {
+// 		type: Sequelize.STRING,
+// 		allowNull: false,
+// 	},
+// 	description: {
+// 		type: Sequelize.STRING,
+// 		allowNull: false,
+// 	},
+// });
 
 module.exports = Product;
