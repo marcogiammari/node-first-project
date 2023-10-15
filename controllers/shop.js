db882df51d27b4423f083989f79ef40835666f0f
Marco Giammari <marcogiammari@gmail.com>
2023-10-15 11:08:52 +0200

    deleting related cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -110,7 +110,22 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.findById(prodId, (product) => {
+	req.user
+		.getCart()
+		.then((cart) => {
+			return cart.getProducts({ where: { id: prodId } });
+		})
+		.then((products) => {
+			const product = products[0];
+			return product.cartItem.destroy();
+		})
+		.then(() => {
+			res.redirect("/cart");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	Product.findByPk(prodId, (product) => {
 		Cart.deleteProduct(prodId, product.price);
 		res.redirect("/cart");
 	});
